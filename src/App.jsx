ff485493999465fe1e87d4c22ce65e371ed6ca70
Minh Doan <minh.nghia.doan.2006@gmail.com>
2025-02-05 18:26:37 -0500

    created Movie Rater Project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import MovieItem from "./MovieItem";
+import { useState } from "react";
+
+function App() {
+  return <>
+  <h1>Movie Rater</h1>
+  </>
+}
+
+export default App;
---
+++ src/MovieItem.jsx
@@ -0,0 +1,12 @@
+import {useState} from 'react'
+
+function MovieItem() {
+
+    return <div class="movieContainer">
+        <img src="src\assets\react.svg" />
+        <h3 class="movieName">MOVIE NAME</h3>
+        <p class="movieDesc">Description Here</p>
+    </div>
+}
+
+export default MovieItem;
