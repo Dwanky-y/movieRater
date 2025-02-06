92522dea343f76c0723a27b173b81179ee444c84
Minh Doan <minh.nghia.doan.2006@gmail.com>
2025-02-06 00:34:30 -0500

    added useStateArray

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import './App.css' //adds styling from App.css
 import MovieItem from "./MovieItem";
 import { useState } from "react";
 
+//https://education.github.com/git-cheat-sheet-education.pdf
 function App() {
+  
+  const [movies, setMovies] = useState([
+    <MovieItem movieName = {"Cool Movie"} movieDesc={"This is a cool movie!"}></MovieItem>,
+    <MovieItem movieName = {"Cool Movie 2"} movieDesc={"This is a cool movie!"}></MovieItem>,
+    <MovieItem movieName = {"Cool Movie 33"} movieDesc={"This is a movie!"}></MovieItem>
+  ])
+
   return <>
   <h1 id="title">Movie Rater</h1>
   <div id="container">
-    <MovieItem movieName = {"Cool Movie"} movieDesc={"This is a cool movie!"}></MovieItem>
-    <MovieItem movieName = {"Cool Movie"} movieDesc={"This is a cool movie!"}></MovieItem>
-    <MovieItem movieName = {"Cool Movie"} movieDesc={"This is a cool movie!"}></MovieItem>
-    <MovieItem movieName = {"Cool Movie"} movieDesc={"This is a cool movie!"}></MovieItem>
+    {movies.map((item) => item)}
   </div>
   
   </>
