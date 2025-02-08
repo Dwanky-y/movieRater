6ec2822ea967a988a5dd4ab98d54000d24599bb4
Minh Doan <minh.nghia.doan.2006@gmail.com>
2025-02-07 21:26:14 -0500

    code refactoring, added rank to movies

--- src/MovieItem.jsx
+++ src/MovieItem.jsx
@@ -1,13 +1,20 @@
 import './MovieItem.css'
 import {useState} from 'react'
 
-function MovieItem({movieImg, movieName, movieDesc}) {
+function MovieItem({movieImg, movieName, movieDesc, rank}) {
 
-    return <div class="movieContainer">
+    return <>
+    
+    <div className="movieContainer">
+        <h3 class="movieRank">{rank}</h3>
         <img src={movieImg ? movieImg :'src\\assets\\react.svg'} />
-        <h3 class="movieName">{movieName ? movieName : "MOVIE NAME"}</h3>
-        <p class="movieDesc">{movieDesc ? movieDesc : "Description Here"}</p>
+        <div class="movieDetails">
+            <h3 className="movieName">{movieName ? movieName : "MOVIE NAME"}</h3>
+            <p className="movieDesc">{movieDesc ? movieDesc : "Description Here"}</p>
+        </div>
     </div>
+    </>
+
 }
 
 export default MovieItem;
